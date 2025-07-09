82e6747c564449aec3b450e9ba8389759adef576
meet_2107 <mbhavsar590@gmail.com>
2025-07-09 21:59:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const app = express();
  const PORT =process.env.PORT || 5000 ;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors({
+  origin: 'https://meetkasodariya.github.io/hiring_page/'
+}));
 // Database connection
 connectDB();
 app.get('/', (req, res) => {
