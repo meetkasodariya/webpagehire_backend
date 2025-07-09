26933c7362211c63226223e24369ab6ceac6b4b7
meetkasodariya <mbhavsar590@gmail.com>
2025-07-09 12:39:12 +0530

    addd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const connectDB = require('./config/db');
 const developersRouter = require('./routes/developers');
 require('dotenv').config();
 const app = express();
-// const PORT = 5000;
+ const PORT =process.env.PORT || 5000 ;
 
 // Middleware
 app.use(cors());
@@ -23,6 +23,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT }`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
